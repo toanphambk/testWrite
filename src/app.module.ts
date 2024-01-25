6f78956c70c00391404063af63cc9ec6d8eb0fc2
toanphambk <toanphambk@gmail.com>
2024-01-25 13:55:01 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PlcCommunicationModule } from './plc-communication/plc-communication.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PlcCommunicationModule],
+  imports: [EventEmitterModule.forRoot(), PlcCommunicationModule],
 })
 export class AppModule {}
