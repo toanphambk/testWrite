9639c357ac49effdb0073afd612e3d889ff45239
toanphambk <toanphambk@gmail.com>
2024-01-25 14:03:18 +0700

    first commit

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -7,7 +7,7 @@ const configuration: Configuration = {
       type: 'WRITE_ONLY',
     },
     barcodeFlag: {
-      address: 'DB40,INT0.1',
+      address: 'DB47,INT0.1',
       type: 'READ_WRITE',
     },
   },
